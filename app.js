ccc16b16a4b75b16a95b572809994ece94983e15
Sandhya312 <sandhyakumari3122003@gmail.com>
2024-01-27 17:38:50 +0530

    added cors

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }
+));
+
 
 
 // Routes
