45fcf50c6c3d9ca7b49dee88abd28505a7346ada
Sandhya312 <sandhyakumari3122003@gmail.com>
2024-02-18 01:36:58 +0530

    modify quiz analytics function

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -175,7 +175,7 @@ const addQuizAnalytics = asyncHandler(async(req,res)=>{
     }
     quiz.impressions= quiz_impression;
     await quiz.save();
-    res.status(constants.SUCCESS).send(quiz);
+    res.status(constants.SUCCESS).send(quiz.impressions);
 })
 
 
